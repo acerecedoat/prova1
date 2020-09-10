1cdf9d21dbec5dba026aeccc36a986d820deac4a
Aleix Cerecedo <acerecedo@atsistemas.com>
2020-09-10 16:00:55 +0200

    all uncommented

--- src/main/java/org/al/prova1/DatabaseDao.java
+++ src/main/java/org/al/prova1/DatabaseDao.java
@@ -35,16 +35,16 @@ public class DatabaseDao {
 	public String doTest () {
 		String msg = "";
 		try {
-//			// TEST REPO
-//			ObjectRepository<GithubRow_v1> repository = db.getRepository(GithubRow_v1.class);
-//	    	repository.insert(new GithubRow_v1("2020-07", 0.8));
-//	    	System.out.println("inserted");
-//	    	msg += "<br/>inserted";
-//	    	List<GithubRow_v1> list = repository.find(ObjectFilters.eq("month", "2020-07")).toList();
-//	    	for (GithubRow_v1 row : list) {
-//	    		System.out.println("row: "+row);
-//	    		msg += "<br/>"+row;
-//	    	}
+			// TEST REPO
+			ObjectRepository<GithubRow_v1> repository = db.getRepository(GithubRow_v1.class);
+	    	repository.insert(new GithubRow_v1("2020-07", 0.8));
+	    	System.out.println("inserted");
+	    	msg += "<br/>inserted";
+	    	List<GithubRow_v1> list = repository.find(ObjectFilters.eq("month", "2020-07")).toList();
+	    	for (GithubRow_v1 row : list) {
+	    		System.out.println("row: "+row);
+	    		msg += "<br/>"+row;
+	    	}
 
 	    	db.getCollection("github_co").insert(Document.createDocument("month", "2020-07").put("value", 0.7));
 	    	db.getCollection("github_co").insert(Document.createDocument("month", "2020-08").put("value", 0.8));
