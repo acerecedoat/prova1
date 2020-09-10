846f12c67d605e6995ed8fe6ed02c05d553f9462
Aleix Cerecedo <acerecedo@atsistemas.com>
2020-09-10 12:37:38 +0200

    mongodo

--- src/main/java/org/al/prova1/ProvaController.java
+++ src/main/java/org/al/prova1/ProvaController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -44,4 +50,41 @@ class ProvaController {
     	LOG.info("Hi");
     	return ResponseEntity.ok("Success.");
 	}
+    
+    @GetMapping("/csv")
+    public ResponseEntity<String> list() {
+    	
+    	LOG.info("Hi");
+    	
+    	MongoClient mongoClient = new MongoClient("localhost", 27017);
+    	DB database = mongoClient.getDB("myMongoDb");
+    	database.createCollection("customers", null);
+    	
+    	DBCollection collection = database.getCollection("customers");
+    	BasicDBObject document = new BasicDBObject();
+    	document.put("name", "Shubham");
+    	document.put("company", "Baeldung");
+    	collection.insert(document);
+    	
+    	BasicDBObject query = new BasicDBObject();
+    	query.put("name", "Shubham");
+    	 
+    	BasicDBObject newDocument = new BasicDBObject();
+    	newDocument.put("name", "John");
+    	 
+    	BasicDBObject updateObject = new BasicDBObject();
+    	updateObject.put("$set", newDocument);
+    	 
+    	collection.update(query, updateObject);
+    	
+    	BasicDBObject searchQuery = new BasicDBObject();
+    	searchQuery.put("name", "John");
+    	DBCursor cursor = collection.find(searchQuery);
+    	 
+    	while (cursor.hasNext()) {
+    	    System.out.println(cursor.next());
+    	}
+    	
+    	return ResponseEntity.ok("Success.");
+    }
 }
